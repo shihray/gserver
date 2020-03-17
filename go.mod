1263e1598e4dc1c0b51304725be5f7ab1986de53
shihray <diskahoy@gmail.com>
2020-03-17 22:44:53 +0800

    [update] remove unuse package

--- go.mod
+++ go.mod
@@ -3,15 +3,12 @@ module github.com/shihray/gserver
 go 1.13
 
 require (
-	github.com/BurntSushi/toml v0.3.1
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
 	github.com/coreos/etcd v3.3.18+incompatible // indirect
-	github.com/garyburd/redigo v1.6.0
 	github.com/golang/protobuf v1.3.4
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/gorilla/websocket v1.4.1
-	github.com/hashicorp/consul/api v1.4.0
 	github.com/joho/godotenv v1.3.0
 	github.com/mitchellh/hashstructure v1.0.0
 	github.com/nats-io/nats-server/v2 v2.1.4 // indirect
--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/nats-io/nats.go"
 	"net/http"
 	_ "net/http/pprof"
 
 	_ "github.com/joho/godotenv/autoload"
 
-	ping "github.com/shihray/gserver/demoPING"
-	pong "github.com/shihray/gserver/demoPONG"
 	Module "github.com/shihray/gserver/module"
-	Conf "github.com/shihray/gserver/utils/conf"
 
 	moduleUtil "github.com/shihray/gserver/source/moduleutil"
 )
@@ -35,27 +31,4 @@ func ListenServe() {
 
 func main() {
 	ListenServe()
-	// nats setting
-	natsUrl := Conf.GetEnv("NatsURL", nats.DefaultURL)
-	nc, err := nats.Connect(natsUrl, nats.MaxReconnects(10000))
-	if err != nil {
-		fmt.Println("Nats Connect Error ", err.Error())
-		return
-	}
-	fmt.Println("Connect to Nats Server... ", nc.ConnectedAddr())
-
-	app := CreateApp(
-		Module.Version(version), // version
-		Module.Nats(nc),         // nats register
-	)
-
-	erro := app.Run(
-		ping.Module(),
-		pong.Module(),
-	)
-	if erro != nil {
-		fmt.Println("App Work[Run] Error", erro.Error())
-		return
-	}
-	fmt.Println("Start Server...", app)
 }
